9954109123ac2e44c43d2c6862704b5dbc41f397
Santiago Speroni <speronisantiago@gmail.com>
2022-07-18 23:35:46 -0300

    Agregando Nabar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import './Navbar.css';
+
+const Navbar = () => {
+    return ( 
+        <nav> 
+            <div> Raizar Beauty</div>
+            <div>
+                <button>Skincare</button>   
+                <button>Makeup</button> 
+                <button>Contacto</button> 
+            </div>
+        </nav>
+    )
+
+};
+
+export default Navbar;
